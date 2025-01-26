1d67eab351e8e205731c486f5e5ad3f2e4414260
adri6400 <bernardadr@cy-tech.fr>
2025-01-26 16:26:21 +0000

    fvdsc

--- api/scraping_api.py
+++ api/scraping_api.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
